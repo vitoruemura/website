499ddb0889e988a6f3e482c17db8755278c3475b
vitoruemura <vitoruemura@gmail.com>
2024-08-14 11:29:08 -0300

    finalmente

--- splide.js
+++ splide.js
@@ -3,3 +3,27 @@ new Splide( '#slider1' ).mount();
 new Splide( '#slider2' ).mount();
 
 new Splide( '#slider3' ).mount();
+
+new Splide( '#slider4' ).mount();
+
+new Splide( '#slider5' ).mount();
+
+new Splide( '#slider6' ).mount();
+
+new Splide( '#slider7' ).mount();
+
+new Splide( '#slider8' ).mount();
+
+new Splide( '#slider9' ).mount();
+
+new Splide( '#slider10' ).mount();
+
+new Splide( '#slider11' ).mount();
+
+new Splide( '#slider12' ).mount();
+
+new Splide( '#slider13' ).mount();
+
+new Splide( '#slider14' ).mount();
+
+new Splide( '#slider15' ).mount();
